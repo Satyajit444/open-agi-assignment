a25450f9f0ef9dfd93e148959d41ed993a900eb1
satyajit444777 <satyajit.senapati@nettantra.net>
2024-10-05 21:32:30 +0530

    added api response

--- src/components/OpenAIComponent.js
+++ src/components/OpenAIComponent.js
@@ -3,14 +3,52 @@ import { getOpenAIResponse } from "./openaiService";
 
 const OpenAIComponent = () => {
   const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [history, setHistory] = useState([]);
   const [response, setResponse] = useState("");
 
+  //   const handleSubmit = async (e) => {
+  //     e.preventDefault();
+  //     const aiResponse = await getOpenAIResponse(input);
+  //     console.log("🚀 ~ handleSubmit ~ aiResponse:", aiResponse);
+  //     // setResponse(aiResponse.choices[0].text);
+  //   };
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const aiResponse = await getOpenAIResponse(input);
-    setResponse(aiResponse.choices[0].text);
-  };
+    const prompt = {
+      role: "user",
+      content: input,
+    };
 
+    setResponse([...response, prompt]);
+
+    await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer 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`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        messages: [...messages, prompt],
+      }),
+    })
+      .then((data) => data.json())
+      .then((data) => {
+        console.log("🚀 ~ .then ~ data:", data)
+        const res = data.choices[0].message.content;
+        console.log("🚀 ~ .then ~ res:", res)
+        setMessages((messages) => [
+          ...messages,
+          {
+            role: "assistant",
+            content: res,
+          },
+        ]);
+        setHistory((history) => [...history, { question: input, answer: res }]);
+        setInput("");
+      });
+  };
   return (
     <div>
       <h1>OpenAI Integration with React</h1>
@@ -27,7 +65,7 @@ const OpenAIComponent = () => {
       </form>
       <div>
         <h2>Response:</h2>
-        <p>{response}</p>
+        {/* <p>{response}</p> */}
       </div>
     </div>
   );
