699a72ae2ca0a7c37eb9ac94aa2585fe0ea8f19f
satyajit444777 <satyajit.senapati@nettantra.net>
2024-10-06 22:52:26 +0530

    updated response form

--- src/components/Output.jsx
+++ src/components/Output.jsx
@@ -1,14 +1,37 @@
 import React from "react";
 import { useFormContext } from "./context/FormContext";
+import OutPutIcon from "./icons/OutPutIcon";
+import DotIcon from "./icons/DotIcon";
 
 export const Output = () => {
   const { response } = useFormContext();
 
   return (
-    <div>
-      <h2>API Response</h2>
-      <textarea value={response} readOnly />
+    <div className="rounded-lg shadow-lg">
+      <div className="flex justify-between py-2.5 px-4 items-center text-black font-semibold text-sm">
+        <div className="flex justify-center items-center gap-3">
+          <OutPutIcon />
+          OUTPUt
+        </div>
+        <DotIcon />
+      </div>
+
+      <div className="px-4 py-2.5 bg-[#EEF4FF] text-[#666666] font-medium text-sm">
+        {"Lorem ipsum sic dolar amet "}
+      </div>
+      <div className="px-4 pt-2.5 pb-4 flex items-start flex-col">
+        <label className="pb-2 text-[#000000] text-sm ">Output Response</label>
+        <textarea
+          value={response}
+          readOnly
+          className="border-[1px] rounded-[4px] border-solid border-[#666666] px-3 py-1 text-sm font-normal focus:outline-none"
+          placeholder="Output response will be shown here"
+          required
+        />
+      </div>
+      <div className="pt-2 px-4 pb-6 text-xs text-[#666666] flex justify-start font-medium items-start">
+        LLM Engine
+      </div>
     </div>
   );
 };
-
